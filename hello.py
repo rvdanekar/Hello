49b9e7d3aef88ac76648588215a0f60bd57ab824
rvdanekar <rvdanekar@gmail.com>
2026-04-27 15:26:37 +0530

    Add separator lines before and after greeting

--- hello.py
+++ hello.py
@@ -3,4 +3,6 @@ from datetime import datetime
 
 user = os.environ.get("USERNAME", "User")
 now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print("====================")
 print(f"Hello {user}! Today is {now}")
+print("====================")
